a3f3ab0eb083a80c81ab92773ab8ab63739255e2
Marcus Svartmark <marcus.svartmark@redpill-linpro.com>
2016-03-23 15:47:00 +0100

    Added share console page for showing statistics Signed-off-by:Marcus Svartmark <marcus.svartmark@redpill-linpro.com>

---
+++ share/src/main/resources/alfresco/site-webscripts/org/redpill/alfresco/console/statistics/user-statistics-console.get.js
@@ -0,0 +1,13 @@
+/**
+ * Admin Console Statistics component
+ */
+
+function main() {
+  // Widget instantiation metadata...
+  var widget = {
+    id : "UserStatisticsConsole", 
+    name : "RL.UserStatisticsConsole"
+  };
+  model.widgets = [widget];
+  }
+main();
