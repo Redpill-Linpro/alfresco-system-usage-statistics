17e99e816f7aff1dba4eb914ec87420367c804c4
Jimmie Aleksic <jimmie@redpill-linpro.com>
2016-10-13 11:21:28 +0200

    #35709 Implemented UI for user email statistics

---
+++ share/src/main/resources/alfresco/site-webscripts/org/redpill/alfresco/console/statistics/user-email-statistics-console.get.js
@@ -0,0 +1,13 @@
+/**
+ * Admin Console Statistics component
+ */
+
+function main() {
+  // Widget instantiation metadata...
+  var widget = {
+    id : "UserUserEmailStatisticsConsole", 
+    name : "RL.UserEmailStatisticsConsole"
+  };
+  model.widgets = [widget];
+  }
+main();
